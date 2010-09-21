45de7e2d01578083f9e5a2953e21c1a14f4ee819
Hannes Wallnöfer <hannesw@gmail.com>
2010-09-21 16:49:19 +0200

    serve larger body in hello-world

--- hello-world/node.js
+++ hello-world/node.js
@@ -1,7 +1,13 @@
 var http = require('http');
+var Buffer = require('buffer').Buffer;
+
+var n = 1024;
+var b = new Buffer(n);
+for (var i = 0; i < n; i++)
+    b[i] = 100;
 
 http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("hello world");
+    res.end(b);
 }).listen(8001);
 
--- hello-world/ringo.js
+++ hello-world/ringo.js
@@ -1,11 +1,17 @@
+var {ByteArray} = require('binary');
 var {Server} = require('ringo/httpserver');
+
+var n = 1024;
+var b = new ByteArray(n);
+for (var i = 0; i < n; i++)
+    b[i] = 100;
 
 Server({
     app: function(request) {
         return {
             status: 200,
             headers: {"Content-Type": "text/plain"},
-            body: ["hello world"]
+            body: [b]
         };
     },
     port: 8001
